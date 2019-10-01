dde7675e5d066f97f0ea12adb5352e8f612241c2
Joanna Damianakis <z5162567@ad.unsw.edu.au>
2019-10-01 15:42:43 +1000

    Addition of 12hr time format included

--- app/src/main/java/com/example/homeworktask1/MainActivity.java
+++ app/src/main/java/com/example/homeworktask1/MainActivity.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-import java.util.Calendar;
-import java.time.LocalDateTime;
-import java.util.TimeZone;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -48,25 +49,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //modifying the Paris included layout
         Paris = findViewById(R.id.Paris);
         city1 = Paris.findViewById(R.id.city);
         city1.setText("Paris");
         parisPic = Paris.findViewById(R.id.cityPic);
         parisPic.setImageResource(R.drawable.paris);
+        time1 = Paris.findViewById(R.id.time12);
+        time1.setText(get12Time("Europe/Paris"));
 
         London = findViewById(R.id.London);
         city2 = London.findViewById(R.id.city);
         city2.setText("London");
         londonPic = London.findViewById (R.id.cityPic);
         londonPic.setImageResource(R.drawable.london);
+        time2 = London.findViewById(R.id.time12);
+        time2.setText(get12Time("Europe/London"));
 
         Sydney = findViewById(R.id.Sydney);
         city3 = Sydney.findViewById(R.id.city);
         city3.setText("Sydney");
         sydneyPic = Sydney.findViewById (R.id.cityPic);
         sydneyPic.setImageResource(R.drawable.sydney);
+        time3 = Sydney.findViewById(R.id.time12);
+        time3.setText(get12Time("Australia/Sydney"));
 
 
         NewYork = findViewById(R.id.NewYork);
@@ -74,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
         city4.setText("New York");
         newYorkPic = NewYork.findViewById (R.id.cityPic);
         newYorkPic.setImageResource(R.drawable.newyork);
+        time4 = NewYork.findViewById(R.id.time12);
+        time4.setText(get12Time("America/New_York"));
 
 
         Amsterdam = findViewById(R.id.Amsterdam);
@@ -81,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
         city5.setText("Amsterdam");
         amsterdamPic = Amsterdam.findViewById (R.id.cityPic);
         amsterdamPic.setImageResource (R.drawable.amsterdam);
+        time5 = Amsterdam.findViewById(R.id.time12);
+        time5.setText(get12Time("Europe/Amsterdam"));
 
 
         Dublin = findViewById(R.id.Dublin);
@@ -88,8 +98,17 @@ public class MainActivity extends AppCompatActivity {
         city6.setText("Dublin");
         dublinPic = Dublin.findViewById (R.id.cityPic);
         dublinPic.setImageResource (R.drawable.dublin);
+        time6 = Dublin.findViewById(R.id.time12);
+        time6.setText(get12Time("Europe/Dublin"));
 
+    }
 
+    public static String get12Time(String timeZone){
+        ZoneId zoneId = ZoneId.of(timeZone);
+        LocalTime localTime=LocalTime.now(zoneId);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm: a");
+        String formattedTime = localTime.format(formatter);
+        return formattedTime;
     }
 
 }
---
+++ app/src/main/java/com/example/homeworktask1/findTime.java
@@ -0,0 +1,9 @@
+package com.example.homeworktask1;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class findTime {
+
+
+}
